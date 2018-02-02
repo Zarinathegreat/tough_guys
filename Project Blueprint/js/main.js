4e4317398a4a552698d2db838b316028e57ae17f
Zarina <zarinathegreat@gmail.com>
2018-02-02 23:07:49 +0500

    Added some CSS animation

--- Project Blueprint/js/main.js
+++ Project Blueprint/js/main.js
@@ -1,29 +1,27 @@
 $(document).ready (function() {
-    
-    new WOW().init();
-    
+     
     
    $('.js-wp-2').waypoint(function(direction) {
        
        $('.js-wp-2').addClass('animated slideInUp');
        
    }, {
-		offset: '70%'
+		offset: '20%'
 	});
     
-    $('.js-wp-3').waypoint(function(direction) {
+//     $('.js-wp-3').waypoint(function(direction) {
        
-       $('.js-wp-3').addClass('animated fadeIn');
+//        $('.js-wp-3').addClass('animated fadeIn');
        
-   }, {
-		offset: '70%'
-	});
+//    }, {
+// 		offset: '70%'
+// 	});
     
     
     
     
-    $(".iphone-btn").delay(2300).animate( {bottom: "+=-3"}, 300);
-    $(".iphone-btn").delay(300).animate( {top: "+=-3"}, 100);
+//     $(".iphone-btn").delay(2300).animate( {bottom: "+=-3"}, 300);
+//     $(".iphone-btn").delay(300).animate( {top: "+=-3"}, 100);
     
     
     
